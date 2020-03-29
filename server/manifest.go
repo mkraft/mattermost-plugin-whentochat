0d2568581890563ba836efcc84c4285d3906f667
Martin Kraft <martin@upspin.org>
2020-03-29 07:52:24 -0400

    Removes unused files.

--- server/manifest.go
+++ server/manifest.go
@@ -12,9 +12,9 @@ var manifest *model.Manifest
 
 const manifestStr = `
 {
-  "id": "com.mattermost.plugin-starter-template",
-  "name": "Plugin Starter Template",
-  "description": "This plugin serves as a starting point for writing a Mattermost plugin.",
+  "id": "org.upspin.plugin-whentochat",
+  "name": "When To Chat",
+  "description": "Display the best time to chat across time zones.",
   "version": "0.1.0",
   "min_server_version": "5.12.0",
   "server": {
@@ -24,9 +24,6 @@ const manifestStr = `
       "windows-amd64": "server/dist/plugin-windows-amd64.exe"
     },
     "executable": ""
-  },
-  "webapp": {
-    "bundle_path": "webapp/dist/main.js"
   },
   "settings_schema": {
     "header": "",
